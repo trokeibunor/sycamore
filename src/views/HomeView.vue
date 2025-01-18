24282e59aa986dd9dd4480d15b0ec99411e7440f
Okeibunor Emmanuel <okeibunoremma@gmail.com>
2025-01-18 08:28:18 +0100

    fix: remove vitest

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,13 @@
       <div class="content w-full h-full flex items-center justify-center flex-col gap-4">
         <img src="../assets/empty_page.svg" class="w-full h-[30vh]" alt="" />
         <h5 class="text-center text-md">Hey there's nothing here, check the customer page</h5>
-        <router-link to="/customers" class="text-blue-500 text-sm">Go to Customer Page</router-link>
+        <div class="flex gap-2 items-center">
+          <router-link to="/customers" class="text-blue-500 text-sm"
+            >Go to Customer Page</router-link
+          >
+          <p>or</p>
+          <a class="text-blue-500 text-sm" href="http://">Check out the Codily Test</a>
+        </div>
       </div>
     </div>
   </main>
